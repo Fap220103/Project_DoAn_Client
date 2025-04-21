43ec069830067f03feb5196a70a26d825ac47a6d
LQTrung05 <lqtrungfcb@gmail.com>
2025-04-21 17:37:52 +0700

    chore: add client ui

--- src/app/modules/client/client-routing.module.ts
+++ src/app/modules/client/client-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ClientComponent } from './client.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: ClientComponent,
+    children: [{ path: 'home', component: HomeComponent }]
   }
 ];
 
--- src/app/modules/client/component/navbar/navbar.component.ts
+++ src/app/modules/client/component/navbar/navbar.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  isLoggedIn: boolean = false;
+  constructor(private authService: AuthService) {
+    this.isLoggedIn = authService.isLoggedIn();
+  }
 
   ngOnInit() {}
 }
