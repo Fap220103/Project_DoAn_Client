34f61b612b8334bf25847467b18c880df2ff22e7
LQTrung05 <lqtrungfcb@gmail.com>
2025-04-21 16:52:23 +0700

    chore: add translate

--- src/app/core/constants/common.ts
+++ src/app/core/constants/common.ts
@@ -1,7 +1,7 @@
 export const PriceFields = [
-  { name: 'originalPrice', label: 'Giá nhập' },
-  { name: 'price', label: 'Giá bán' },
-  { name: 'salePercent', label: 'Phần trăm khuyến mãi' }
+  { name: 'originalPrice', label: 'Product.OriginalPrice' },
+  { name: 'price', label: 'Product.Price' },
+  { name: 'salePercent', label: 'Product.SalePercent' }
 ];
 
 export const optionsCheckbox = [{ name: 'isActive', label: 'Hiển thị' }];
